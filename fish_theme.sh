255921aa73bdb408fbb08a72fa10ded66a243d2e
ikura-hamu <104292023+ikura-hamu@users.noreply.github.com>
2025-12-05 23:43:30 +0900

    :bug: スクリプト修正

--- fish_theme.sh
+++ fish_theme.sh
@@ -6,4 +6,4 @@ curl -L \
   -H "Accept: application/vnd.github+json" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   https://api.github.com/repos/$REPO_NAME/tarball/HEAD \
-  | tar xz --strip-components=1 --wildcards '*/functions/*' -C ~/.config/fish/
+  | tar xz -C ~/.config/fish --strip-components=1 --wildcards '*/functions/*' 
